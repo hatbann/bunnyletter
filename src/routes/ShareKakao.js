be7684018bfd179f130360e0410c16f2463e9108
hatbann <0urloveis_great@naver.com>
2023-01-16 16:37:31 +0900

    Feat:카카오공유하기버튼추가

--- src/routes/ShareKakao.js
+++ src/routes/ShareKakao.js
@@ -10,12 +10,33 @@ const ShareKakao = () => {
   const bunnyCard = location.state;
   const imgURL = window.URL.createObjectURL(bunnyCard);
 
+  const onClickShare = () => {
+    if (window.Kakao) {
+      const kakao = window.Kakao;
+
+      if (!kakao.isInitialized()) {
+        kakao.init('d97b7b839267dc8a1120ffca40fbf8d3');
+      }
+
+      kakao.Link.createCustomButton({
+        container: '#kakaotalk-sharing-btn',
+        templateId: 88708,
+        templateArgs: {
+          image: imgURL,
+        },
+      });
+    }
+  };
+
   return (
     <>
       <Nav />
       <div className="section">
         <img src={imgURL} alt="" id="bunnyBlobImg" />
       </div>
+      <button onClick={onClickShare} id="kakaotalk-sharing-btn">
+        공유하기
+      </button>
     </>
   );
 };
